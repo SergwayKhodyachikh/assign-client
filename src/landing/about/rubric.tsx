2927ee7d97c7a9b4182414a64ee76d52c6ad1034
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2021-01-16 13:19:44 +0200

    add the about section to the landing page

---
+++ src/landing/about/rubric.tsx
@@ -0,0 +1,72 @@
+// .about__rubric {
+
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  illustration: string;
+  title: string;
+  paragraph: string;
+}
+
+const Wrapper = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    &:not(:last-child) {
+      margin-bottom: 5rem;
+    }
+    ${theme.breakpoints.down('sm')} {
+      justify-content: 'center';
+      align-items: center;
+      margin: 3rem;
+      margin-top: 5rem;
+    }
+  `}
+`;
+const Image = styled.img.attrs({
+  alt: 'about-paragraph',
+})`
+  ${({ theme }) => css`
+    display: none;
+    width: 100%;
+    max-width: 50rem;
+    margin-bottom: 2rem;
+    ${theme.breakpoints.down('sm')} {
+      display: block;
+    }
+  `}
+`;
+const Title = styled.h3`
+  ${({ theme }) => css`
+    font-size: 2.1rem;
+    font-weight: 700;
+    text-transform: capitalize;
+    letter-spacing: 0.1rem;
+    margin-bottom: 1rem;
+
+    ${theme.breakpoints.down('md')} {
+      font-size: 1.7rem;
+    }
+  `}
+`;
+const Paragraph = styled.p`
+  ${({ theme }) => css`
+    font-size: 1.6rem;
+    line-height: 1.7;
+    font-weight: 400;
+    ${theme.breakpoints.down('md')} {
+      font-size: 1.4rem;
+    }
+  `}
+`;
+
+export const Rubric: React.FC<Props> = ({ illustration, title, paragraph }) => {
+  return (
+    <Wrapper>
+      <Image src={illustration} />
+      <Title>{title}</Title>
+      <Paragraph>{paragraph}</Paragraph>
+    </Wrapper>
+  );
+};
--- src/landing/index.tsx
+++ src/landing/index.tsx
@@ -1,11 +1,12 @@
 import React, { Fragment } from 'react';
+import { About } from './about';
 import { Header } from './header';
 
 export const Landing: React.FC = () => {
   return (
     <Fragment>
       <Header />
-      <div>about</div>
+      <About />
       <div>features</div>
       <div>scroll button</div>
     </Fragment>
